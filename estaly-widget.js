02065fc793af4f6379fde5ae5e22d5bc07598107
kakou100 <karim@estaly.co>
2022-09-01 18:12:46 +0200

    event

--- estaly-widget.js
+++ estaly-widget.js
@@ -79,10 +79,10 @@ const PDP = {
         })
 
         const addToCartButton = document.getElementsByClassName(ADD_TO_CART_CLASS_NAME)[0]
-        addToCartButton.addEventListener("click", () => {
+        addToCartButton.addEventListener("click", (event) => {
             if (this.selectedPlanId == null) {
             } else {
-                this.addOfferToCart(variantReferenceId, e);
+                this.addOfferToCart(variantReferenceId, event);
             }
         })
     },
