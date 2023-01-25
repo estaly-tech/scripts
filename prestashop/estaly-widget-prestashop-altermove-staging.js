6a0f6338c84fc95cdeefe453a4d4ece4c9b7e9c7
kakou100 <karim@estaly.co>
2023-01-25 11:22:57 +0100

    update creation estaly_insurance_matchings

--- prestashop/estaly-widget-prestashop-altermove-staging.js
+++ prestashop/estaly-widget-prestashop-altermove-staging.js
@@ -194,7 +194,7 @@ const PDP = {
                 $.ajax({
                     type: 'POST',
                     headers: { "cache-control": "no-cache" },
-                    url: "http://localhost:8888/prestashop/module/estalymodule/insurancematching",
+                    url: "https://clone.altermove.com/module/estalymodule/insurancematching",
                     async: false,
                     cache: false,
                     dataType : "json",
