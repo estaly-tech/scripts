2bd258d1384f12d9bcec735278587cdcf0c9a4c8
kakou100 <karim@estaly.co>
2023-02-15 16:24:36 +0100

    test 10

--- estaly-widget-zeride.js
+++ estaly-widget-zeride.js
@@ -81,11 +81,11 @@ const PDP = {
         console.log("VARIANTREFERENCEID");
         const variantReferenceId = evt.currentTarget.estalyVariantSelected;
         console.log(variantReferenceId);
-        if (this.selectedPlanId === null) {
+        if (PDP.selectedPlanId === null) {
             console.log("NULL");
             return
         }
-        jQuery.ajax({url: '/wp/?post_type=product&add-to-cart='+this.selectedPlanId+'&productVariantId='+variantReferenceId,
+        jQuery.ajax({url: '/wp/?post_type=product&add-to-cart='+PDP.selectedPlanId+'&productVariantId='+variantReferenceId,
             async: false
         });
     }, 
